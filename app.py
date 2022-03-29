0389a20bc8b74806e6839573e8685cd284dfaed3
Pavel Popov <pavelpopov@Pavels-MBP.wifi.gsu.edu>
2022-03-29 18:59:16 -0400

    best branch

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ params = {
 # best comment fasfdfwgehwgwrhwrgrg
 response = requests.get(BASE_URL, params=params)
 data = response.json()
-headlines = []
+headlines = ["h", "hi"]
 
 for i in range(0, 10):
     print(data["response"]["docs"][i]["headline"]["main"])
